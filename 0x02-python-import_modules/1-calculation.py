5a0495b4928078b5233af45e6871c4aaa02e2098
malidifour <malidifour@gmail.com>
2022-06-05 07:34:06 -0700

    advance

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from calculator_1.py import div, mul, add, sub
+from calculator_1 import div, mul, add, sub
 
-if __nmae__ == "__main__":
+if __name__ == "__main__":
     a = 10
     b = 5
-
+    
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
