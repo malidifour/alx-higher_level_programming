c03d5acc7d83964f2517d407915200cb8f070f45
malidifour <malidifour@gmail.com>
2022-06-12 12:16:17 -0700

    concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpretend, interactive, object-oriented programming\
         language that combines remarkable power with veryclear syntax"
-str = str[39:66]+' '+str[107:111]+' '+str[:6]
+str = str[39:67]+ str[114:120]+ str[:6]
 print(str)
